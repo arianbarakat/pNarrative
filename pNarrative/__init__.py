8e32b859d636e3e0d0491345a3616f0cfe215fe3
Arian Barakat <arianbarakat@gmail.com>
2018-10-31 13:15:30 +0100

    Bug fixes for making installation work

--- pNarrative/__init__.py
+++ pNarrative/__init__.py
@@ -1,5 +1,1 @@
-from __future__ import absolute_import
-import pNarrative.parser
-#from util import normalization
-import pNarrative.gp
-import pNarrative.kernels
+
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
 
 setup(name='pNarrative',
       version='0.1',
       description='A python module to estimate the narrative arcs in literature',
       url='',
       author='Arian Barakat',
+      long_description=long_description,
       author_email='arianbarakat@gmail.com',
       license='MIT',
-      packages=['pNarrative'],
-      install_requires=[
-          're',
-          'numpy',
-          'inspect',
-          'matplotlib.pyplot'
-      ],
+      packages=find_packages(),
+      include_package_data=True,
+      #install_requires=['re','numpy','matplotlib'],
       zip_safe=False)
