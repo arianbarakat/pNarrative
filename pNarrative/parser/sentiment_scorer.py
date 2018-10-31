862a0e42baeda9eec08fd3eed6af8d919239f927
Arian Barakat <arianbarakat@gmail.com>
2018-10-31 14:18:49 +0100

    Added get_sentiment_dictionary function + edited requirements and setup.py

--- pNarrative/parser/sentiment_scorer.py
+++ pNarrative/parser/sentiment_scorer.py
@@ -15,6 +15,34 @@ def create_lexicon(path_to_file, delim):
 
 
 
+def get_sentiment_lexicon(lexicon, lang):
+    """
+    Get a sentiment lexicon provided by the module
+    """
+
+    import os
+    import pNarrative
+    existing_lexicons = {"afinn":["sv","en"]}
+    filename = {"afinn":\
+                    {"en":'/data/lexicons/afinn/AFINN-en-165.txt',\
+                     "sv":'/data/lexicons/afinn/AFINN-sv-165.txt'}}
+
+    assert isinstance(lexicon, str)
+    assert isinstance(lang, str)
+    assert lexicon in existing_lexicons.keys(), "Lexicon not supported yet. Check spelling?"
+    assert lang in existing_lexicons[lexicon], "Language not supported yet. Check spelling? Remember to use ISO-country code"
+
+    path_base = os.path.dirname(pNarrative.__file__)
+    path_file = filename[lexicon][lang]
+    path_full = path_base + path_file
+    print(path_full)
+    lexicon_dictionary = create_lexicon(path_to_file= path_full ,delim="\t")
+    return lexicon_dictionary
+
+
+
+
+
 def get_sentiment_score(tokens, lexicon):
     assert isinstance(lexicon, dict)
     scores = [lexicon.get(token, float(0)) for token in tokens]
